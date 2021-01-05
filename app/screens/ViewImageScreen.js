4d95ff25077d76d5b95a47438ecfe8e7120c56c7
phemmy <adexphem4real@gmail.com>
2021-01-05 21:34:19 +0100

    Using material community icons on viewimagescreen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import colors from '../config/colors'
 
 const ViewImageScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon} />
-      <View style={styles.deleteIcon} />
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" size={30} color="white"/>
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons name="trash-can-outline" size={30} color="white" />
+      </View>
       <Image resizeMode='contain' style={styles.image} source={require("../assets/chair.jpg")} />
     </View>
   );
@@ -15,9 +20,6 @@ const ViewImageScreen = () => {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30
@@ -27,9 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30
