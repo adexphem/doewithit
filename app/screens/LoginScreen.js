c7439b03029bb3c173b6bb009503225a9b29c115
phemmy <adexphem4real@gmail.com>
2021-01-10 00:27:10 +0100

    Building Form Component and refactor usage of Formkit

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,57 +1,48 @@
-import React from 'react';
-import { Image, StyleSheet, View } from 'react-native'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import * as Yup from "yup";
 
-import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
-import Screen from '../components/Screen';
-import ErrorMessage from '../components/ErrorMessage';
-import AppFormField from '../components/AppFormField';
-import SubmitButton from '../components/SubmitButton';
+import Screen from "../components/Screen";
+import AppFormField from "../components/AppFormField";
+import SubmitButton from "../components/SubmitButton";
+import AppForm from "../components/AppForm";
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().required().email().label('Email'),
-  password: Yup.string().required().min(4).max(20).label('Password'),
-})
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).max(20).label("Password"),
+});
 
 function LoginScreen(props) {
   return (
     <Screen>
-      <Image 
-        style={styles.logo}
-        source={require('../assets/logo-red.png')} 
-      />
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
 
-      <Formik
-          initialValues={{email: "", password: ""}}
-          onSubmit={values => console.log(values)}
-          validationSchema={validationSchema}
-        >
-          {() => (
-            <View style={styles.container}>
-              <AppFormField
-                autoCapitalise="none"
-                autoCorrect={false}
-                icon="email"
-                name="email"
-                keyboardType="email-address"
-                placeholder="Email"
-                textContentType="emailAddress"
-              />
-              <AppFormField
-                autoCapitalise="none"
-                autoCorrect={false}
-                icon="lock"
-                name="password" 
-                placeholder="Password "
-                secureTextEntry
-                textContentType="password"
-              />
-              <SubmitButton title="Login" />
-            </View>
-          )}
-        </Formik>
+      <AppForm
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}>
+        <View style={styles.container}>
+          <AppFormField
+            autoCapitalise='none'
+            autoCorrect={false}
+            icon='email'
+            name='email'
+            keyboardType='email-address'
+            placeholder='Email'
+            textContentType='emailAddress'
+          />
+          <AppFormField
+            autoCapitalise='none'
+            autoCorrect={false}
+            icon='lock'
+            name='password'
+            placeholder='Password '
+            secureTextEntry
+            textContentType='password'
+          />
+          <SubmitButton title='Login' />
+        </View>
+      </AppForm>
     </Screen>
   );
 }
@@ -59,21 +50,21 @@ function LoginScreen(props) {
 const styles = StyleSheet.create({
   invalid: {
     color: "red",
-    fontSize: 11,
+    fontSize: 12,
     marginTop: -10,
-    paddingLeft: 40
+    paddingLeft: 40,
   },
   logo: {
     width: 80,
     height: 80,
     alignSelf: "center",
     marginTop: 50,
-    marginBottom: 30
+    marginBottom: 30,
   },
   container: {
     width: "85%",
-    alignSelf: "center"
-  }
-})
+    alignSelf: "center",
+  },
+});
 
 export default LoginScreen;
