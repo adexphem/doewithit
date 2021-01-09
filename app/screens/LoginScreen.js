12c9a434cfb552e83b5dc03c848ca1217298177b
phemmy <adexphem4real@gmail.com>
2021-01-09 20:19:38 +0100

    Building Login Screen

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, View } from 'react-native'
+import AppButton from '../components/AppButton';
+import AppTextInput from '../components/AppTextInput';
+import Screen from '../components/Screen';
+
+function LoginScreen(props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <Screen>
+      <Image 
+        style={styles.logo}
+        source={require('../assets/logo-red.png')} 
+      />
+
+      <View style={styles.container}>
+        <AppTextInput
+          autoCapitalise="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          onChangeText={value => setEmail(value)}
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <AppTextInput
+          autoCapitalise="none"
+          autoCorrect={false}
+          icon="lock"
+          onChangeText={value => setPassword(value)}
+          placeholder="Password "
+          secureTextEntry
+          textContentType="password"
+        />
+        <AppButton 
+          onPress={() => {}}
+          title="Login" 
+        />
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20
+  },
+  container: {
+    width: "85%",
+    alignSelf: "center"
+  }
+})
+
+export default LoginScreen;
