345421528d2cdc18fb40364a1989691914100c3a
phemmy <adexphem4real@gmail.com>
2021-01-09 01:50:53 +0100

    Building the Listing screen

---
+++ app/screens/ListingScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+import Screen from '../components/Screen'
+import Card from '../components/Card'
+import colors from '../config/colors'
+
+const listings = [
+  {
+    id: 1,
+    title: "Nice looking couch for sale",
+    price: 2300,
+    image: require('../assets/couch.jpg')
+  },
+  {
+    id: 2,
+    title: "Almost new Dinning set",
+    price: 1900,
+    image: require('../assets/dinning.jpg')
+  },
+  {
+    id: 3,
+    title: "Folding Functional Sofa Bed Fashion Bunk Bed ",
+    price: 14900,
+    image: require('../assets/reclining.jpg')
+  }
+]
+
+function ListingScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => 
+          <Card
+            title={item.title}
+            subTitle={`$${item.price}`}
+            image={item.image}
+          />
+        }
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light
+  }
+})
+
+export default ListingScreen;
