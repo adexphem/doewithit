14afd64f309b5636ca3767731ff0edb5597fca32
phemmy <adexphem4real@gmail.com>
2021-01-14 00:32:29 +0100

    Requesting user device permissions

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as ImagePicker from 'expo-image-picker'
+
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if(!granted) {
+      alert("Oops! You need to enable permission to access the library.")
+    }
+  }
+  useEffect(() => {
+    requestPermission();
+  }, [])
   return (
     <ListingEditScreen />
   );
