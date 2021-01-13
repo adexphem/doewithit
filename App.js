a1012aed2c14c409941cd7e8c0da29d3888a0812
phemmy <adexphem4real@gmail.com>
2021-01-14 00:55:25 +0100

    using Permission Module

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from "react";
 import * as ImagePicker from 'expo-image-picker'
+import * as Permissions from 'expo-permissions'
 
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const { granted } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY)
+    
     if(!granted) {
       alert("Oops! You need to enable permission to access the library.")
     }
   }
+  
   useEffect(() => {
     requestPermission();
   }, [])
