c1839ff387009ef602fddcfaa8dd1ce88b79816a
phemmy <adexphem4real@gmail.com>
2021-01-15 00:22:26 +0100

    Add Scrolling ability on the ImageInputList Component

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import ImageInput from './ImageInput';
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage}) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {
-        imageUris.map(uri => 
-          <View key={uri} style={styles.image}>
-            <ImageInput 
-              imageUri={uri} 
-              onChangeImage={() => onRemoveImage(uri)} 
-            />
-          </View>
-        )
-      }
-      <ImageInput onChangeImage={uri => onAddImage(uri)} />
+    <View>
+      <ScrollView ref={scrollView} horizontal onContentSizeChange={() => scrollView.current.scrollToEnd()}>
+        <View style={styles.container}>
+          {
+            imageUris.map(uri => 
+              <View key={uri} style={styles.image}>
+                <ImageInput 
+                  imageUri={uri} 
+                  onChangeImage={() => onRemoveImage(uri)} 
+                />
+              </View>
+            )
+          }
+          <ImageInput onChangeImage={uri => onAddImage(uri)} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -25,7 +31,7 @@ const styles = StyleSheet.create({
     flexDirection: "row"
   },
   image: {
-    marginRight: 15
+    marginRight: 10
   }
 })
 
