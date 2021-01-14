09a1cfa6b18183f64d0c191a8b8308d2f286ac14
phemmy <adexphem4real@gmail.com>
2021-01-14 01:13:17 +0100

    Accessing library, select and show image selected in the app

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
+import { Button, Image } from "react-native";
 
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+
 
 export default function App() {
+  const [imageUri, setImageUri] = useState()
   const requestPermission = async () => {
-    const { granted } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY)
-    
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if(!granted) {
       alert("Oops! You need to enable permission to access the library.")
     }
   }
-  
+
   useEffect(() => {
     requestPermission();
   }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if(!result.cancelled) {
+        const { uri } = result;
+        setImageUri(uri)
+      }
+    } catch (error) {
+      console.log("Could not fetch image ", error)
+    }
+  }
+
   return (
-    <ListingEditScreen />
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{uri: imageUri}} style={{width: 200, height: 200}} />
+    </Screen>
   );
 }
