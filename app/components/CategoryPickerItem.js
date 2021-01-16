21601743d79b84ecd99a5c9f493dddc8e5e8a1fc
phemmy <adexphem4real@gmail.com>
2021-01-16 13:30:53 +0100

    How to get users location

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import AppText from './AppText';
-import Icon from './Icon';
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import AppText from "./AppText";
+import Icon from "./Icon";
 
-function CategoryPickerItem({item, onPress}) {
+function CategoryPickerItem({ item, onPress }) {
   return (
-    <View style={styles.container} onPress={() => onPress(item)}>
-      <Icon name={item.icon} backgroundColor={item.backgroundColor} size={80}/>
-      <AppText style={styles.label}>{item.label}</AppText>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon name={item.icon} backgroundColor={item.backgroundColor} size={80} />
+        <AppText style={styles.label}>{item.label}</AppText>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,11 +19,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingVertical: 15,
     alignItems: "center",
-    width: '33%'
+    width: "33%",
   },
   label: {
     marginTop: 5,
-    textAlign: "center"
-  }
-})
+    textAlign: "center",
+  },
+});
 export default CategoryPickerItem;
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import * as Yup from "yup";
+import * as Location from 'expo-location'
 
 import Screen from "../components/Screen";
 import { AppForm, AppFormField, AppFormPicker, SubmitButton } from "../components/forms";
-import AppPicker from "../components/AppPicker";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
 
@@ -17,17 +17,86 @@ const validationSchema = Yup.object().shape({
 });
 
 const categories = [
-  { label: "Furniture", value: 1, backgroundColor: 'red', icon: 'apps' },
-  { label: "Clothing", value: 2, backgroundColor: 'green', icon: 'email' },
-  { label: "Camera", value: 3, backgroundColor: 'blue', icon: 'lock' },
+  {
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    backgroundColor: "#fd9644",
+    icon: "car",
+    label: "Cars",
+    value: 2,
+  },
+  {
+    backgroundColor: "#fed330",
+    icon: "camera",
+    label: "Cameras",
+    value: 3,
+  },
+  {
+    backgroundColor: "#26de81",
+    icon: "cards",
+    label: "Games",
+    value: 4,
+  },
+  {
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
+    value: 5,
+  },
+  {
+    backgroundColor: "#45aaf2",
+    icon: "basketball",
+    label: "Sports",
+    value: 6,
+  },
+  {
+    backgroundColor: "#4b7bec",
+    icon: "headphones",
+    label: "Movies & Music",
+    value: 7,
+  },
+  {
+    backgroundColor: "#a55eea",
+    icon: "book-open-variant",
+    label: "Books",
+    value: 8,
+  },
+  {
+    backgroundColor: "#778ca3",
+    icon: "application",
+    label: "Other",
+    value: 9,
+  },
 ];
 
 function ListingEditScreen(props) {
+  const [location, setLocation] = useState()
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestPermissionsAsync();
+    if(!granted) return;
+
+    const {coords: {longitude, latitude} } = await Location.getLastKnownPositionAsync();
+    setLocation({latitude, longitude})
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, [])
+
+  const handleFormSubmit = (data) => {
+    console.log('---- ', data)
+  }
+
   return (
     <Screen>
       <AppForm
         initialValues={{ title: "", price: "", description: "", category: null, images: [] }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => handleFormSubmit({values, location})}
         validationSchema={validationSchema}>
         <View style={styles.container}>
           <FormImagePicker name="images" />
